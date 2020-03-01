e6e613b457207468eb97d27de0ed795fe0528851
ShinyGreenRobot <henrik.samuelsson@gmail.com>
2020-03-01 10:11:02 +0100

    Change to use images to present result of dice roll.

--- dice-roller/app/src/main/java/com/samdide/android/diceroller/MainActivity.kt
+++ dice-roller/app/src/main/java/com/samdide/android/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.samdide.android.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import kotlin.random.Random
@@ -12,7 +13,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener {
             rollDice()
@@ -20,8 +20,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
+        val diceImage: ImageView = findViewById(R.id.dice_image)
         val randomInt = Random.nextInt(6) + 1
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = randomInt.toString()
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
     }
 }
