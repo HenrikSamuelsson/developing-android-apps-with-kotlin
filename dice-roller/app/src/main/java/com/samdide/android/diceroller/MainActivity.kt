f5a327afe5f3f184a8277f5f67ddfd2cf8ad6a5f
ShinyGreenRobot <henrik.samuelsson@gmail.com>
2020-06-05 18:29:57 +0200

    Add the property lateinit to the diceImage variable.

--- dice-roller/app/src/main/java/com/samdide/android/diceroller/MainActivity.kt
+++ dice-roller/app/src/main/java/com/samdide/android/diceroller/MainActivity.kt
@@ -10,6 +10,8 @@ import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,10 +19,11 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener {
             rollDice()
         }
+
+        diceImage = findViewById(R.id.dice_image)
     }
 
     private fun rollDice() {
-        val diceImage: ImageView = findViewById(R.id.dice_image)
         val randomInt = Random.nextInt(6) + 1
         val drawableResource = when (randomInt) {
             1 -> R.drawable.dice_1
